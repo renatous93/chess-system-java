e19820fc40807cbcecd817a1d2e514b479247ec1
Renato Teixeira <renatous93@gmail.com>
2023-08-14 13:35:26 +0100

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
+import boardgame.position;
+
 public class Program {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello world");
+		position pos = new position(3, 5);
+		System.out.println(pos);
 	}
 
 }
